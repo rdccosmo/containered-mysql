f8a02b1be4151f21fefe510c15e40486adf50cca
Rodrigo Cosme <rdccosmo@gmail.com>
2016-09-28 10:50:07 -0300

    Fix blocking call to stream log file to stdout
    Reference:
    - http://serverfault.com/questions/599103/make-a-docker-application-write-to-stdout

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mysql:5.6
 COPY init.sh /init.sh
 RUN chmod +x /init.sh
-RUN /init.sh
 
 COPY ./container-files /
+ENTRYPOINT ["/init.sh"]
--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -eu
 
 MYSQL_BIND_ADDRESS=${MYSQL_BIND_ADDRESS:-0.0.0.0}
 
 sed -i -e"s/^#bind-address\s*=\s*127.0.0.1/bind-address = ${MYSQL_BIND_ADDRESS}/" /etc/mysql/my.cnf
+
+tail -F /var/lib/mysql/queries.log &
+
+exec /usr/local/bin/docker-entrypoint.sh mysqld
