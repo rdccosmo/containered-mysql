50ab4f4d52759240da483b64e60db96893ec8cec
Rodrigo Cosme <rdccosmo@gmail.com>
2019-10-11 14:48:53 -0300

    create queries.log file before tailing it

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -eu
 MYSQL_BIND_ADDRESS=${MYSQL_BIND_ADDRESS:-0.0.0.0}
 
 sed -i -e"s/^#bind-address\s*=\s*127.0.0.1/bind-address = ${MYSQL_BIND_ADDRESS}/" /etc/mysql/my.cnf
-
+touch /var/lib/mysql/queries.log
 tail -F /var/lib/mysql/queries.log &
 
 exec /usr/local/bin/docker-entrypoint.sh mysqld
